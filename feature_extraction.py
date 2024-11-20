cc67c7774c7bf43c1c7163872b1540058ab6a8e9
ANAS2119 <90722425+ANAS2119@users.noreply.github.com>
2024-11-20 14:54:38 -0500

    feature extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,35 +5,26 @@ from scipy.signal import welch
 import pandas as pd
 
 
-def extract_features(file_name):
-    try:
-        with open(file_name, 'rb') as f:
-            data = pickle.load(f, encoding='latin1')
-    except FileNotFoundError:
-        print(f"Error: The file '{file_name}' was not found.")
-        return
-    except Exception as e:
-        print(f"Error: {e}")
-        return
-
+def features_extraction_(data):
+    
     print((data.keys()))
     array_of_data = []
-    array_of_signal_types = []
 
+    #df prepration
     columnlabels = ["signal_type", "snr", "magnitude_mean", "magnitude_std", "magnitude_skew", "magnitude_kurtosis", "phase_mean", "phase_std", "phase_skew", "phase_kurtosis", "spectral_entropy", "peak_frequency", "average_power"]
-    for key in data.keys():
+    for key in data.keys(): # key dict has 220 len.
         signal_type = key[0]
         # print(signal_type)
-        array_of_signal_types.append(signal_type)
         if signal_type in ["BPSK","QPSK","QAM16","WBFM","GFSK"]:
             snr = key[1]
             if int(snr) >= 0:
                 samples = data[key]
-                for i in range(len(samples)):
-                    i_samples = samples[i][0]
-                    q_samples = samples[i][1]
-                    magnitude = np.sqrt(i_samples**2 + q_samples**2)
-                    phase = np.arctan2(q_samples, i_samples)
+                # calculate the magnitude anf phase for each signal (sample)
+                for i in range(len(samples)): # each i represent a sample or signal, total 1000 signals
+                    i_samples = samples[i][0] # each sample has 128 bits for real part
+                    q_samples = samples[i][1] # each sample has 128 bits for imaginery part
+                    magnitude = np.sqrt(i_samples**2 + q_samples**2) #array with len 128
+                    phase = np.arctan2(q_samples, i_samples) # array with len 128
 
                     # Calculate statistical features for magnitude
                     magnitude_mean = np.mean(magnitude)
@@ -47,6 +38,7 @@ def extract_features(file_name):
                     phase_skew = skew(phase)
                     phase_kurtosis = kurtosis(phase)
 
+                    # calculate the spectral entropy
                     frequencies, psd = welch(magnitude, nperseg=128)
                     psd_norm = psd / np.sum(psd)
                     spectral_entropy = entropy(psd_norm)
@@ -54,9 +46,13 @@ def extract_features(file_name):
                     peak_frequency = frequencies[np.argmax(psd)]
 
                     average_power = np.mean(psd)
+
+                    # fill the data array ( array_of_data)
                     array_of_data.append([signal_type, snr, magnitude_mean, magnitude_std, magnitude_skew, magnitude_kurtosis, phase_mean, phase_std, phase_skew, phase_kurtosis, spectral_entropy, peak_frequency, average_power])
-    df = pd.DataFrame(array_of_data, columns = columnlabels)
-    # df.to_csv("extracted_features_5_types_snr_gt_zero.csv", index=False)
-    unique_signal_types = set(array_of_signal_types)
-    print(unique_signal_types)
+    
+    df = pd.DataFrame(array_of_data, columns = columnlabels) # define the complete dataset with rows (samples) and columns ( features)
+
+    # create a csv file 
+    df.to_csv("extracted_features_5_types_snr_gt_zero.csv", index=False)
+    
     return df
